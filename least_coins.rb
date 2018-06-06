ebb9e8486df334abdee44954ac3eb45660598347
Alysa Barbarisi <baal02@cox.net>
2018-06-06 23:22:51 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -5,21 +5,25 @@ coins = {}
  amount = cents
  if amount >= 25
    coins["quarters"] = (amount - amount.modulo(25)) / 25
+   amount = amount.modulo(25)
  else
    coins["quarters"] = 0 
  end
  if amount >= 10 
    coins["dimes"] = (amount - amount.modulo(10)) / 10
+   amount = amount.modulo(10)
  else
    coins["dimes"] = 0 
  end
  if amount >= 5 
    coins["nickels"] = (amount - amount.modulo(5)) / 5 
+   amount = amount.modulo(5)
  else
    coins["nickels"] = 0 
  end
  if amount > 0  
    coins["pennies"] = amount 
+   amount = amount.modulo(1)
  
  end
  
